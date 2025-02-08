8c00a8ff373be27f52d8ab56b9333a307b4aaf72
iiiboston <111111+iiiboston@users.noreply.github.com>
2025-02-07 22:01:01 -0500

    test commit: add lint-staged setup

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,1 +1,19 @@
- 
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:prettier/recommended'
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  settings: { react: { version: '18.2' } },
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off',
+    'no-unused-vars': 'warn'
+  }
+} 
